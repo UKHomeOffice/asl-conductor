cdad578cbf2d51455aa51ce6ad02f20907ab40f7
Phil Moorhouse <phil@wheelsandcogs.co.uk>
2018-08-16 17:10:53 +0100

    don't need to create additional users per db

--- .docker/postgres/create_dbs.sh
+++ .docker/postgres/create_dbs.sh
@@ -2,20 +2,18 @@
 set -e
 set -u
 
-function create_user_and_database() {
+function create_database() {
   local database=$1
-  echo " Creating user and database '$database'"
+  echo " Creating database '$database'"
   psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE USER $database;
-    CREATE DATABASE $database;
-    GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
+    CREATE DATABASE "$database";
 EOSQL
 }
 
 if [ -n "$POSTGRES_DATABASES" ]; then
   echo "Multiple database creation requested: $POSTGRES_DATABASES"
   for db in $(echo $POSTGRES_DATABASES | tr ',' ' '); do
-    create_user_and_database $db
+    create_database $db
   done
-  echo "Multiple databases created"
+  echo "All databases created"
 fi
